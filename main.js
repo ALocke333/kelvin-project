215b4e25e03895a51e0d1f152b964bf15e96b40d
ALocke333 <104593944+ALocke333@users.noreply.github.com>
2022-04-28 10:39:48 -1000

    Create main.js

---
+++ main.js
@@ -0,0 +1,18 @@
+// temperature in kelvin
+const kelvin = 293
+//  temperature converted from kelvin to celsius
+var celsius = (kelvin - 273);
+//  temperature converted from celsius to fahrenheit
+var fahrenheit = (celsius * (9/5) + 32) ;
+
+console.log(celsius * (9/5) + 32);
+// temperature in fahrenheit
+var fahrenheit = 68
+// fahrenheit temperature rounded down
+console.log(Math.floor(fahrenheit));
+
+var fahrenheit = 68
+
+console.log(`The temperature is '${fahrenheit}' degrees fahrenheit`);
+
+// 0 kelvin in fahrenheit is -459.4
